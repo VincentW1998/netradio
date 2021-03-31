590ce473570b6d2a27451e8d9cea6b10cc1edb8f
ngof <felix.ngo@etu.univ-paris-diderot.fr>
2021-03-31 16:20:10 +0200

    fix IMALIVE

--- code/Diffuser.java
+++ code/Diffuser.java
@@ -5,9 +5,6 @@ public class Diffuser {
     private String id;
     private InetAddress ip1, ip2;
     private int port1, port2;
-    private Socket gestionnaire;
-    // private BufferedReader br;
-    // private PrintWriter pw;
 
     public Diffuser(String identifiant, InetAddress ipAdress1, int p1, InetAddress ipAdress2, int p2) {
         id = identifiant;
@@ -15,7 +12,6 @@ public class Diffuser {
         ip2 = ipAdress2;
         port1 = p1;
         port2 = p2;
-    
     }
 
     public static String getFullIp(InetAddress ip) {
@@ -30,21 +26,14 @@ public class Diffuser {
         return id + " " + getFullIp(ip1) + " " + String.valueOf(port1) + " " + getFullIp(ip2) + " " + String.valueOf(port2);
     }
 
-    public void getRegistered(Socket gestionnaire){ // will try to register until REOK is received
+    public void getRegistered(BufferedReader br, PrintWriter pw){ // will try to register until REOK is received
         try{   
-            BufferedReader br = new BufferedReader(new InputStreamReader(gestionnaire.getInputStream()));
-            PrintWriter pw = new PrintWriter(new OutputStreamWriter(gestionnaire.getOutputStream()));
             pw.print("REGI "+toString()+"\n");
             pw.flush();
-            String message;
-            while((message = br.readLine()) == null);
+            String message = br.readLine();
             System.out.println("message received : "+message);
-            if(message.equals("REOK"))
-                return;
-            getRegistered(gestionnaire);
-            // message = br.readLine();
-            // System.out.println(message);
-            // System.out.println("finished");
+            if(!message.equals("REOK"))
+                getRegistered(br, pw);
         }
         catch(Exception e){
             System.out.println("registration error");
@@ -67,45 +56,25 @@ public class Diffuser {
         try{
             assert args.length != 2;
             int p = Integer.parseInt(args[1]);
-            ServerSocket server = connectToAvailablePort(9998);
-            Socket diff_gest = new Socket("localhost", p);
-            Diffuser d = new Diffuser(args[0], server.getInetAddress(), p,  server.getInetAddress(), server.getLocalPort()); // not ok broadcast ip and port needs to be changed
-            d.getRegistered(diff_gest);
-            ImAlive ia = new ImAlive(diff_gest);
+            ServerSocket reception = connectToAvailablePort(9998); 
+            Socket connexionToGestionnaire = new Socket("localhost", p);
+            BufferedReader br = new BufferedReader(new InputStreamReader(connexionToGestionnaire.getInputStream()));
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(connexionToGestionnaire.getOutputStream()));
+            Diffuser d = new Diffuser(args[0], reception.getInetAddress(), p,  reception.getInetAddress(), reception.getLocalPort()); // not ok broadcast ip and port needs to be changed
+            d.getRegistered(br, pw); //Diffuser registration
+            ImAlive ia = new ImAlive(br, pw);
             Thread imAliveCheck = new Thread(ia);
             imAliveCheck.start();
             while(true){
-                Socket client = server.accept();
+                Socket client = reception.accept();
                 Service_Diffuser SD = new Service_Diffuser(client);
                 System.out.println("New connection detected");
                 Thread t = new Thread(SD);
                 t.start();
             }
-
-
         }
         catch(Exception e){
 
         }
-        
-
     }
-
-    // public static void main(String [] args) {
-
-    //     try{
-    //         DatagramSocket udpsocket = new DatagramSocket();
-    //         byte[]data;
-    //         for(int i=0;i <= 10; i++){
-    //             String s="MESSAGE "+i+" \n";
-    //             data=s.getBytes();
-    //             InetSocketAddress ia = new InetSocketAddress("localhost",9998);
-    //             DatagramPacket paquet = new DatagramPacket(data,data.length, ia);
-    //             udpsocket.send(paquet);
-    //         }
-    //         udpsocket.close();
-    //     } catch(Exception e){
-    //         e.printStackTrace();
-    //     }
-    // }
 }
